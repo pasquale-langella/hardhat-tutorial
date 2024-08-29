9dc06516e142ceb4311763f91a3282a0bc839b21
Pasquale Langella <paslangella@gmail.com>
2024-08-29 16:06:21 +0200

    change Token.sol contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,6 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.0;
 
-
 // This is the main building block for smart contracts.
 contract Token {
     // Some string type variables to identify the token.
